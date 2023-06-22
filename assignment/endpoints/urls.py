26d27ad94af7186534be9ea6c0af84522e4abb33
bwilkins96 <ben.jr.speed@gmail.com>
2023-06-22 15:38:28 -0400

    assignment views

--- assignment/endpoints/urls.py
+++ assignment/endpoints/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'endpoints'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('view-1/', views.view_1, name='view_1'),
+    path('view-2/', views.view_2, name='view_2')
 ]
--- assignment/endpoints/views.py
+++ assignment/endpoints/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'endpoints/index.html')
+
+def view_1(request):
+    return render(request, 'endpoints/view_1.html')
+
+def view_2(request):
+    return render(request, 'endpoints/view_2.html')
